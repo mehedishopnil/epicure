e0da2fd987cc63ca8b76c4c1f939f70a8e6fe74a
SK Mehedi Hasan <82324698+mehedishopnil@users.noreply.github.com>
2024-03-16 19:46:21 +0600

    project setup

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from 'react-router-dom';
+import Header from '../pages/Share/Header/Header';
+import Footer from '../pages/Share/Footer/Footer';
 
 const Main = () => {
     return (
-        <div>
+        <div className="w-full bg-[#424242]">
+            <Header/>
             <Outlet></Outlet>
+            <Footer/>
         </div>
     );
 };
