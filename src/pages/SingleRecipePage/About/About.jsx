9ead4bcf45f9771378c3f5e5e9a20f5bc689fe29
SK Mehedi Hasan <82324698+mehedishopnil@users.noreply.github.com>
2024-03-29 17:37:56 +0600

    pasta rose page done

--- src/pages/SingleRecipePage/About/About.jsx
+++ src/pages/SingleRecipePage/About/About.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import Pasta_1 from "../../../assets/images/Pasta-1.png";
+import Pasta_2 from "../../../assets/images/Pasta-1.png";
+import Pasta_3 from "../../../assets/images/Pasta-1.png";
+import Pasta_4 from "../../../assets/images/Pasta-1.png";
 
 const About = () => {
-    return (
-        <div className='container mx-auto'>
-            <h1 className='text-white'>About</h1>
-        </div>
-    );
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-white text-3xl mb-5">About</h1>
+      <p className=" text-[23.33px] p-3 rounded-lg text-[#666666] bg-[#272727] ">
+        Savor the artistry in every bite with my handmade Pasta Rose – a
+        culinary masterpiece born from a passion for pasta. The meticulously
+        crafted petals unfold like a blooming rose, turning a simple meal into a
+        visual and gastronomic sensation. Join me on this journey of taste and
+        aesthetics with my Pasta Rose – a celebration of love, creativity, and
+        the joy of cooking! ✨👩‍
+      </p>
+      <div className="grid grid-cols-4 gap-5 mt-5">
+        <img src={Pasta_1} alt="" />
+        <img src={Pasta_2} alt="" />
+        <img src={Pasta_3} alt="" />
+        <img src={Pasta_4} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/pages/SingleRecipePage/SingleRecipePage.jsx
+++ src/pages/SingleRecipePage/SingleRecipePage.jsx
@@ -27,7 +27,7 @@ const SingleRecipePage = () => {
 
   return (
     <div className=" h-screen bg-[#1E1C1A]">
-      <div className="relative flex  w-full h-[200px] lg:h-[321px] rounded-lg bg-gradient-to-b from-[#768895] mb-5">
+      <div className="relative flex  w-full h-[200px] lg:h-[260px] rounded-lg bg-gradient-to-b from-[#768895] mb-5">
         <div className="absolute flex  bottom-[10%] left-[10%] sm:left-[4%]  md:left-[4%] lg:left-[4%]">
           <div className="flex">
             <img src={pastaImg} alt="" />
