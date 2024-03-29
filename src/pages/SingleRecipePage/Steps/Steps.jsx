db5bbae3d030a57c55086ac4782ed41bd253f061
SK Mehedi Hasan <82324698+mehedishopnil@users.noreply.github.com>
2024-03-29 21:57:37 +0600

    setps section done

--- src/pages/SingleRecipePage/Steps/Steps.jsx
+++ src/pages/SingleRecipePage/Steps/Steps.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+
 
 const Steps = () => {
     return (
-        <div>
-            <h1>Steps</h1>
+        <div className="overflow-y-auto max-h-[400px] scrollbar-none">
+            <h1 className="text-[30.67px] font-bold text-[#D9D9D9]">Steps</h1>
+
+            <div className="grid grid-cols-6 gap-5 mt-5">
+                <div className="w-auto h-[152px] col-span-4 rounded-lg p-3 bg-[#272727]">
+                    <h1 className="text-[20px] font-bold text-[#666666]">Step 1</h1>
+                </div>
+                <div className="col-span-2 rounded-lg p-2 bg-[#272727]">
+                </div>
+            </div>
+
+            <div className="grid grid-cols-6 gap-5 mt-5">
+                <div className="w-auto h-[152px] col-span-4 rounded-lg p-3 bg-[#272727]">
+                    <h1 className="text-[20px] font-bold text-[#666666]">Step 2</h1>
+                </div>
+                <div className="col-span-2 rounded-lg p-2 bg-[#272727]">
+                </div>
+            </div>
+
+            <div className="grid grid-cols-6 gap-5 mt-5">
+                <div className="w-auto h-[152px] col-span-4 rounded-lg p-3 bg-[#272727]">
+                    <h1 className="text-[20px] font-bold text-[#666666]">Step 2</h1>
+                </div>
+                <div className="col-span-2 rounded-lg p-2 bg-[#272727]">
+                </div>
+            </div>
+            
         </div>
     );
 };
