f5e35e06cdf3f067aadfc2aa12100a384e72e101
SK Mehedi Hasan <82324698+mehedishopnil@users.noreply.github.com>
2024-03-29 21:29:08 +0600

    ingredients work done

--- src/pages/SingleRecipePage/About/About.jsx
+++ src/pages/SingleRecipePage/About/About.jsx
@@ -6,7 +6,7 @@ import Pasta_4 from "../../../assets/images/Pasta-1.png";
 const About = () => {
   return (
     <div className="container mx-auto">
-      <h1 className="text-white text-3xl mb-5">About</h1>
+      <h1 className="text-[#D9D9D9] text-3xl mb-5">About</h1>
       <div className="overflow-y-auto h-[400px] scrollbar-none">
       <p className="text-[14px] sm:text-[18px] md:text-[18px] lg:text-[18px] p-3 rounded-lg text-[#666666] bg-[#272727] ">
         Savor the artistry in every bite with my handmade Pasta Rose – a
--- src/pages/SingleRecipePage/Ingredients/Ingredients.jsx
+++ src/pages/SingleRecipePage/Ingredients/Ingredients.jsx
@@ -1,9 +1,46 @@
-import React from 'react';
+
+import ingredientImg1 from "../../../assets/images/ingredient-img1.png";
+import ingredientImg2 from "../../../assets/images/ingredient-img2.png";
 
 const Ingredients = () => {
     return (
         <div>
-            <h2>Ingredients</h2>
+            <h2 className="text-[30.67px] font-bold text-[#D9D9D9]">Ingredients</h2>
+
+            <div className="flex flex-col gap-3 mt-5">
+                <div className="w-10/12 sm:w-2/3 md:w-1/2 lg:w-1/2 flex justify-between  items-center  rounded-xl bg-[#272727] px-4 py-2">
+                    <div className="flex items-center gap-2">
+                    <img src={ingredientImg1} alt="" />
+                    <p className="text-[#666666] font-[poppins]">Bread</p>
+                    </div>
+                    <p className="text-[#A9A9A9] font-[poppins] text-sm">200g</p>
+                </div>
+                
+                <div className="w-10/12 sm:w-2/3 md:w-1/2 lg:w-1/2 flex justify-between items-center  rounded-xl bg-[#272727] px-4 py-2">
+                    <div className="flex items-center gap-2">
+                    <img src={ingredientImg2} alt="" />
+                    <p className="text-[#666666] font-[poppins]">Egg</p>
+                    </div>
+                    <p className="text-[#A9A9A9] font-[poppins] text-sm">200g</p>
+                </div>
+
+                <div className="w-10/12 sm:w-2/3 md:w-1/2 lg:w-1/2 flex justify-between items-center  rounded-xl bg-[#272727] px-4 py-2">
+                    <div className="flex items-center gap-2">
+                    <img src={ingredientImg1} alt="" />
+                    <p className="text-[#666666] font-[poppins]">Milk</p>
+                    </div>
+                    <p className="text-[#A9A9A9] font-[poppins] text-sm">200g</p>
+                </div>
+                
+                <div className="w-10/12 sm:w-2/3 md:w-1/2 lg:w-1/2 flex justify-between items-center rounded-xl bg-[#272727] px-4 py-2">
+                    <div className="flex items-center gap-2">
+                    <img src={ingredientImg2} alt="" />
+                    <p className="text-[#666666] font-[poppins]">Butter</p>
+                    </div>
+                    <p className="text-[#A9A9A9] font-[poppins] text-sm">200g</p>
+                </div>
+
+            </div>
         </div>
     );
 };
