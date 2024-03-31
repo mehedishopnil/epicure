f671bc74eb1de6772f69f74141f9bff2c7fd4d5c
SK Mehedi Hasan <82324698+mehedishopnil@users.noreply.github.com>
2024-04-01 00:37:00 +0600

    search bar

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -13,123 +13,125 @@ import PageHeader from "../../components/common/PageHeader/PageHeader";
 const Search = () => {
   return (
     <div className="w-full h-screen rounded-lg">
-      <div className="h-full rounded-lg bg-[#1E1C1A]">
-        
-        <PageHeader HeaderName="Search" />
+      <div className="grid grid-rows-10 h-full rounded-lg bg-[#1E1C1A]">
+        <div className="row-span-2">
+          <PageHeader HeaderName="Search" />
+        </div>
 
-        {/* Search Bar */}
-        <div className="flex items-center justify-center my-5">
-          <div className="flex items-center justify-center w-full h-[50px] rounded-lg px-3 relative">
-            <input
-              type="text"
-              placeholder="Search for recipes, ingredients, or cooking methods"
-              className="w-[90%] h-full px-10 py-2 rounded-full bg-white outline-none"
-            />
-
-            <div className="absolute top-[50%] right-[10%] transform -translate-y-1/2">
-              <FaSearch className="text-2xl text-gray-500" />
-            </div>
-          </div>
-          </div>
-
-
-        {/* This three section should be scrollable */}
-        <div
-          className=" grid grid-rows-3 overflow-y-auto sm:max-h-[90%] md:max-h-[75%] text-white text-lg px-10 sm:gap-10 lg:gap-10"
-        >
-          {/* tag group 1 */}
-          <div className="row-span-1 ">
-            <h2 className="text-[20px] font-bold text-white">tag group 1</h2>
-            <div className="grid grid-cols-2 overflow-x-auto scrollbar-none scrollbar-thumb-gray-500 scrollbar-track-gray-800 sm:max-h-[60%] md:max-h-[70%] lg:max-h-[80%] sm:grid-cols-2  md:grid-cols-4 lg:grid-cols-5 gap-5  mt-4">
-              <img
-                src={top10Img1}
-                alt="Image 1"
-                className="w-[83px] h-[86px] sm:w-[85px] sm:h-[86px] md:w-[85px] md:h-[85px] lg:w-[85px] lg:h-[85px] mr-4"
+        <div className="row-span-8 ">
+          {/* Search Bar */}
+          <div className="flex items-center justify-center mb-10 my-5">
+            <div className="flex items-center justify-center w-full h-[50px] rounded-lg px-3 relative">
+              <input
+                type="text"
+                placeholder=""
+                className="w-full h-10 px-10 mx-5 py-2 rounded-lg bg-white outline-none"
               />
-              <img
-                src={top10Img2}
-                alt="Image 2"
-                className="w-[83px] h-[86px] sm:w-[85px] sm:h-[86px] md:w-[85px] md:h-[85px] lg:w-[85px] lg:h-[85px] mr-4"
-              />
-              <img
-                src={top10Img3}
-                alt="Image 3"
-                className="w-[83px] h-[86px] sm:w-[85px] sm:h-[86px] md:w-[85px] md:h-[85px] lg:w-[85px] lg:h-[85px] mr-4"
-              />
-              <img
-                src={top10Img4}
-                alt="Image 4"
-                className="w-[83px] h-[86px] sm:w-[85px] sm:h-[86px] md:w-[85px] md:h-[85px] lg:w-[85px] lg:h-[85px] mr-4"
-              />
-              <img
-                src={top10Img5}
-                alt="Image 4"
-                className="w-[83px] h-[86px] sm:w-[85px] sm:h-[86px] md:w-[85px] md:h-[85px] lg:w-[85px] lg:h-[85px] mr-4"
-              />
+              <span className="absolute right-[8%] top-0 bottom-0 flex items-center px-2 text-gray-400">
+                Search
+              </span>
+              <div className="absolute top-[50%] right-[5%] transform -translate-y-1/2">
+                <FaSearch className="text-2xl text-gray-500" />
+              </div>
             </div>
           </div>
 
-          {/* tag group 2 */}
-          <div className="row-span-1 ">
-            <h2 className="text-[20px] font-bold text-white">tag group 2</h2>
-            <div className="grid grid-cols-2  overflow-y-auto scrollbar-none scrollbar-thumb-gray-500 scrollbar-track-gray-800 sm:max-h-[60%] md:max-h-[70%] lg:max-h-[80%] sm:grid-cols-2  md:grid-cols-4 lg:grid-cols-5 gap-5 mt-4">
-              <img
-                src={lastSearch1}
-                alt="Image 5"
-                className="w-[83px] h-[86px] sm:w-[81px] sm:h-[84px] md:w-[81px] md:h-[83px] lg:w-[81px] lg:h-[83px] mr-4"
-              />
-              <img
-                src={lastSearch2}
-                alt="Image 6"
-                className="w-[83px] h-[86px] sm:w-[81px] sm:h-[84px] md:w-[81px] md:h-[83px] lg:w-[81px] lg:h-[83px] mr-4"
-              />
-              <img
-                src={lastSearch3}
-                alt="Image 7"
-                className="w-[83px] h-[86px] sm:w-[81px] sm:h-[84px] md:w-[81px] md:h-[83px] lg:w-[81px] lg:h-[83px] mr-4"
-              />
-              <img
-                src={lastSearch4}
-                alt="Image 8"
-                className="w-[83px] h-[86px] sm:w-[81px] sm:h-[84px] md:w-[81px] md:h-[83px] lg:w-[81px] lg:h-[83px] mr-4"
-              />
-              <img
-                src={top10Img5}
-                alt="Image 4"
-                className="w-[83px] h-[86px] sm:w-[81px] sm:h-[84px] md:w-[81px] md:h-[83px] lg:w-[81px] lg:h-[83px] mr-4"
-              />
+          {/* This three section should be scrollable */}
+          <div className=" grid grid-rows-3 overflow-y-auto sm:max-h-[90%] md:max-h-[75%] text-white text-lg px-10 sm:gap-10 lg:gap-10">
+            {/* tag group 1 */}
+            <div className="row-span-1 ">
+              <h2 className="text-[20px] font-bold text-white">tag group 1</h2>
+              <div className="grid grid-cols-2 overflow-x-auto scrollbar-none scrollbar-thumb-gray-500 scrollbar-track-gray-800 sm:max-h-[60%] md:max-h-[70%] lg:max-h-[80%] sm:grid-cols-2  md:grid-cols-4 lg:grid-cols-5 gap-5  mt-4">
+                <img
+                  src={top10Img1}
+                  alt="Image 1"
+                  className="w-[83px] h-[86px] sm:w-[85px] sm:h-[86px] md:w-[85px] md:h-[85px] lg:w-[85px] lg:h-[85px] mr-4"
+                />
+                <img
+                  src={top10Img2}
+                  alt="Image 2"
+                  className="w-[83px] h-[86px] sm:w-[85px] sm:h-[86px] md:w-[85px] md:h-[85px] lg:w-[85px] lg:h-[85px] mr-4"
+                />
+                <img
+                  src={top10Img3}
+                  alt="Image 3"
+                  className="w-[83px] h-[86px] sm:w-[85px] sm:h-[86px] md:w-[85px] md:h-[85px] lg:w-[85px] lg:h-[85px] mr-4"
+                />
+                <img
+                  src={top10Img4}
+                  alt="Image 4"
+                  className="w-[83px] h-[86px] sm:w-[85px] sm:h-[86px] md:w-[85px] md:h-[85px] lg:w-[85px] lg:h-[85px] mr-4"
+                />
+                <img
+                  src={top10Img5}
+                  alt="Image 4"
+                  className="w-[83px] h-[86px] sm:w-[85px] sm:h-[86px] md:w-[85px] md:h-[85px] lg:w-[85px] lg:h-[85px] mr-4"
+                />
+              </div>
             </div>
-          </div>
 
-          {/* tag group 3 */}
-          <div className="row-span-3 ">
-            <h2 className="text-[20px] font-bold text-white">tag group 3</h2>
-            <div className="grid grid-cols-2 overflow-y-auto scrollbar-none scrollbar-thumb-gray-500 scrollbar-track-gray-800 sm:max-h-[60%] md:max-h-[50%] lg:max-h-[80%] sm:grid-cols-2  md:grid-cols-4 lg:grid-cols-5 gap-5 mt-4">
-              <img
-                src={lastSearch1}
-                alt="Image 5"
-                className="w-[83px] h-[86px] sm:w-[81px] sm:h-[84px] md:w-[81px] md:h-[83px] lg:w-[81px] lg:h-[83px] mr-4"
-              />
-              <img
-                src={lastSearch2}
-                alt="Image 6"
-                className="w-[83px] h-[86px] sm:w-[81px] sm:h-[84px] md:w-[81px] md:h-[83px] lg:w-[81px] lg:h-[83px] mr-4"
-              />
-              <img
-                src={lastSearch3}
-                alt="Image 7"
-                className="w-[83px] h-[86px] sm:w-[81px] sm:h-[84px] md:w-[81px] md:h-[83px] lg:w-[81px] lg:h-[83px] mr-4"
-              />
-              <img
-                src={lastSearch4}
-                alt="Image 8"
-                className="w-[83px] h-[86px] sm:w-[81px] sm:h-[84px] md:w-[81px] md:h-[83px] lg:w-[81px] lg:h-[83px] mr-4"
-              />
-              <img
-                src={top10Img5}
-                alt="Image 4"
-                className="w-[83px] h-[86px] sm:w-[81px] sm:h-[84px] md:w-[81px] md:h-[83px] lg:w-[81px] lg:h-[83px] mr-4"
-              />
+            {/* tag group 2 */}
+            <div className="row-span-1 ">
+              <h2 className="text-[20px] font-bold text-white">tag group 2</h2>
+              <div className="grid grid-cols-2  overflow-y-auto scrollbar-none scrollbar-thumb-gray-500 scrollbar-track-gray-800 sm:max-h-[60%] md:max-h-[70%] lg:max-h-[80%] sm:grid-cols-2  md:grid-cols-4 lg:grid-cols-5 gap-5 mt-4">
+                <img
+                  src={lastSearch1}
+                  alt="Image 5"
+                  className="w-[83px] h-[86px] sm:w-[81px] sm:h-[84px] md:w-[81px] md:h-[83px] lg:w-[81px] lg:h-[83px] mr-4"
+                />
+                <img
+                  src={lastSearch2}
+                  alt="Image 6"
+                  className="w-[83px] h-[86px] sm:w-[81px] sm:h-[84px] md:w-[81px] md:h-[83px] lg:w-[81px] lg:h-[83px] mr-4"
+                />
+                <img
+                  src={lastSearch3}
+                  alt="Image 7"
+                  className="w-[83px] h-[86px] sm:w-[81px] sm:h-[84px] md:w-[81px] md:h-[83px] lg:w-[81px] lg:h-[83px] mr-4"
+                />
+                <img
+                  src={lastSearch4}
+                  alt="Image 8"
+                  className="w-[83px] h-[86px] sm:w-[81px] sm:h-[84px] md:w-[81px] md:h-[83px] lg:w-[81px] lg:h-[83px] mr-4"
+                />
+                <img
+                  src={top10Img5}
+                  alt="Image 4"
+                  className="w-[83px] h-[86px] sm:w-[81px] sm:h-[84px] md:w-[81px] md:h-[83px] lg:w-[81px] lg:h-[83px] mr-4"
+                />
+              </div>
+            </div>
+
+            {/* tag group 3 */}
+            <div className="row-span-3 ">
+              <h2 className="text-[20px] font-bold text-white">tag group 3</h2>
+              <div className="grid grid-cols-2 overflow-y-auto scrollbar-none scrollbar-thumb-gray-500 scrollbar-track-gray-800 sm:max-h-[60%] md:max-h-[50%] lg:max-h-[80%] sm:grid-cols-2  md:grid-cols-4 lg:grid-cols-5 gap-5 mt-4">
+                <img
+                  src={lastSearch1}
+                  alt="Image 5"
+                  className="w-[83px] h-[86px] sm:w-[81px] sm:h-[84px] md:w-[81px] md:h-[83px] lg:w-[81px] lg:h-[83px] mr-4"
+                />
+                <img
+                  src={lastSearch2}
+                  alt="Image 6"
+                  className="w-[83px] h-[86px] sm:w-[81px] sm:h-[84px] md:w-[81px] md:h-[83px] lg:w-[81px] lg:h-[83px] mr-4"
+                />
+                <img
+                  src={lastSearch3}
+                  alt="Image 7"
+                  className="w-[83px] h-[86px] sm:w-[81px] sm:h-[84px] md:w-[81px] md:h-[83px] lg:w-[81px] lg:h-[83px] mr-4"
+                />
+                <img
+                  src={lastSearch4}
+                  alt="Image 8"
+                  className="w-[83px] h-[86px] sm:w-[81px] sm:h-[84px] md:w-[81px] md:h-[83px] lg:w-[81px] lg:h-[83px] mr-4"
+                />
+                <img
+                  src={top10Img5}
+                  alt="Image 4"
+                  className="w-[83px] h-[86px] sm:w-[81px] sm:h-[84px] md:w-[81px] md:h-[83px] lg:w-[81px] lg:h-[83px] mr-4"
+                />
+              </div>
             </div>
           </div>
         </div>
