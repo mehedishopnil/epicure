a191fc2164e3b2413d31eb4396ca407d7e1f9fcb
SK Mehedi Hasan <82324698+mehedishopnil@users.noreply.github.com>
2024-04-09 16:12:37 +0600

    new updae

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,26 +1,23 @@
 import { useState } from "react";
-import { FaGoogle } from "react-icons/fa";
+import { FaGoogle, FaEye, FaEyeSlash } from "react-icons/fa"; // Import eye icons
 import "./Login.css"; // Import CSS file if needed
 import { Link } from "react-router-dom";
 
 const Login = () => {
-  // State variables for email and password
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false); // State to manage password visibility
 
-  // Function to handle form submission
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Here you can add your login logic
     console.log("Login clicked");
     console.log("Email:", email);
     console.log("Password:", password);
   };
 
   return (
-    <div className="h-full rounded-lg  bg-[#1E1C1A]">
-      <div className=" font-syne  h-full rounded-lg lg:grid lg:grid-cols-2 ">
-        {/* Left side: Logo and Tagline */}
+    <div className="h-full rounded-lg bg-[#1E1C1A]">
+      <div className="font-syne h-full rounded-lg lg:grid lg:grid-cols-2">
         <div className="flex flex-col justify-center md:h-[250px] lg:h-full lg:rounded-lg gap-10 p-10 bg-[#BE6F50]">
           <h1 className="md:text-[35px] lg:text-[50px] xl:text-[60px] text-white font-bold">
             Epicure
@@ -29,14 +26,10 @@ const Login = () => {
             Feast Your Senses Culinary Creators on Exclusive Hub
           </h2>
         </div>
-
-        {/* Right side: Login form */}
-        <div className="grid items-center  md:h-[550px] lg:h-full lg:rounded-lg bg-[#1F1D1C]">
+        <div className="grid items-center md:h-[550px] lg:h-full lg:rounded-lg bg-[#1F1D1C]">
           <div className="max-w-md mx-auto p-6">
-            {/* Form title */}
             <h2 className="text-2xl text-white font-bold mb-4">Log in</h2>
-            <form onSubmit={handleSubmit} className=" ">
-              {/* Email input */}
+            <form onSubmit={handleSubmit}>
               <div className="mb-4">
                 <input
                   type="email"
@@ -51,29 +44,34 @@ const Login = () => {
                   required
                 />
               </div>
-              {/* Password input */}
-              <div className="mb-4">
+              <div className="mb-4 relative">
                 <input
-                  type="password"
+                  type={showPassword ? "text" : "password"} // Toggle input type based on showPassword state
                   id="password"
                   name="password"
                   value={password}
                   placeholder="Password"
                   onChange={(e) => setPassword(e.target.value)}
-                  className={`mt-1 p-2 block w-full border border-gray-300 bg-[#8f8f8f00] rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 ${
+                  className={`mt-1 p-2 pr-10 block w-full border border-gray-300 bg-[#8f8f8f00] rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 ${
                     password ? "text-white" : ""
                   }`}
                   required
                 />
+                {/* Eye icon button to toggle password visibility */}
+                <button
+                  type="button"
+                  className="absolute text-[#e1e1e1] inset-y-0 right-0 flex items-center px-3 bg-transparent"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? <FaEyeSlash /> : <FaEye />}
+                </button>
               </div>
-              {/* Submit button */}
               <button
                 type="submit"
                 className="w-full bg-[#8F8F8F] text-white py-2 px-4 rounded-md hover:bg-[#BE6F50] focus:outline-none focus:bg-blue-600"
               >
                 Log in
               </button>
-              {/* Link to signup page */}
               <p className="text-white text-center mt-5">
                 Don't have an account?{" "}
                 <Link to="/signup">
@@ -81,7 +79,6 @@ const Login = () => {
                 </Link>{" "}
               </p>
             </form>
-            {/* Login with Google button */}
             <button className="mt-10 w-full flex items-center justify-center text-white bg-[#BE6F50] py-2 px-4 rounded-full hover:bg-[#8F8F8F] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
               <FaGoogle className="mr-2" /> Log in with Google
             </button>
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,17 +1,15 @@
 import { useState } from "react";
-import { FaGoogle } from "react-icons/fa";
+import { FaGoogle, FaEye, FaEyeSlash } from "react-icons/fa"; // Import eye icons
 import { Link } from "react-router-dom";
 
 const SignUp = () => {
-  // State variables for name, email, and password
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false); // State to manage password visibility
 
-  // Function to handle form submission
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Here you can add your signup logic
     console.log("Signup clicked");
     console.log("Name:", name);
     console.log("Email:", email);
@@ -20,102 +18,96 @@ const SignUp = () => {
 
   return (
     <div className="w-full h-full rounded-lg  bg-[#1E1C1A]">
-      <div className=" font-syne  h-full rounded-lg lg:grid lg:grid-cols-2">
-      {/* Left side: Logo and Tagline */}
-      <div className="flex flex-col justify-center md:h-[250px] lg:h-full lg:rounded-lg gap-10 p-10 bg-[#BE6F50]">
-      <h1 className="md:text-[35px] lg:text-[50px] xl:text-[60px] text-white font-bold">
+      <div className="font-syne h-full rounded-lg lg:grid lg:grid-cols-2">
+        <div className="flex flex-col justify-center md:h-[250px] lg:h-full lg:rounded-lg gap-10 p-10 bg-[#BE6F50]">
+          <h1 className="md:text-[35px] lg:text-[50px] xl:text-[60px] text-white font-bold">
             Epicure
           </h1>
           <h2 className="font-syne lg:text-[35px] xl:text-[45px] text-white">
             Feast Your Senses Culinary Creators on Exclusive Hub
           </h2>
-      </div>
-
-      {/* Right side: Signup form */}
-      <div className="grid items-center  md:h-[550px] lg:h-full lg:rounded-lg bg-[#1F1D1C]">
-        <div className="max-w-md mx-auto p-6">
-          {/* Form title */}
-          <h2 className="text-2xl text-white font-bold mb-4">Sign Up</h2>
-          <form onSubmit={handleSubmit} className="">
-            {/* Name input */}
-            <div className="mb-4">
-              <input
-                type="text"
-                id="name"
-                name="name"
-                placeholder="Name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                className={`mt-1 p-2 block w-full border border-gray-300 bg-[#8f8f8f00] rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 ${
-                  name ? "text-white" : ""
-                }`}
-                required
-              />
-            </div>
-            {/* Email input */}
-            <div className="mb-4">
-              <input
-                type="email"
-                id="email"
-                name="email"
-                value={email}
-                placeholder="Email"
-                onChange={(e) => setEmail(e.target.value)}
-                className={`mt-1 p-2 block w-full border border-gray-300 bg-[#8f8f8f00] rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 ${
-                  email ? "text-white" : ""
-                }`}
-                required
-              />
-            </div>
-            {/* Password input */}
-            <div className="mb-4">
-              <input
-                type="password"
-                id="password"
-                name="password"
-                value={password}
-                placeholder="Password"
-                onChange={(e) => setPassword(e.target.value)}
-                className={`mt-1 p-2 block w-full border border-gray-300 bg-[#8f8f8f00] rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 ${
-                  password ? "text-white" : ""
-                }`}
-                required
-              />
-            </div>
-            {/* Submit button */}
-            <button
-              type="submit"
-              className="w-full bg-[#8F8F8F] text-white py-2 px-4 rounded-md hover:bg-[#BE6F50] focus:outline-none focus:bg-blue-600"
-            >
-              Sign up
+        </div>
+        <div className="grid items-center md:h-[550px] lg:h-full lg:rounded-lg bg-[#1F1D1C]">
+          <div className="max-w-md mx-auto p-6">
+            <h2 className="text-2xl text-white font-bold mb-4">Sign Up</h2>
+            <form onSubmit={handleSubmit} className="">
+              <div className="mb-4">
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  className={`mt-1 p-2 block w-full border border-gray-300 bg-[#8f8f8f00] rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 ${
+                    name ? "text-white" : ""
+                  }`}
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={email}
+                  placeholder="Email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  className={`mt-1 p-2 block w-full border border-gray-300 bg-[#8f8f8f00] rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 ${
+                    email ? "text-white" : ""
+                  }`}
+                  required
+                />
+              </div>
+              <div className="mb-4 relative">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  id="password"
+                  name="password"
+                  value={password}
+                  placeholder="Password"
+                  onChange={(e) => setPassword(e.target.value)}
+                  className={`mt-1 p-2 block w-full border border-gray-300 bg-[#8f8f8f00] rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 ${
+                    password ? "text-white" : ""
+                  }`}
+                  required
+                />
+                {/* Eye icon button to toggle password visibility */}
+                <button
+                  type="button"
+                  className="absolute inset-y-0 right-0 flex items-center px-3 bg-transparent text-[#e1e1e1]"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? <FaEyeSlash /> : <FaEye />}
+                </button>
+              </div>
+              <button
+                type="submit"
+                className="w-full bg-[#8F8F8F] text-white py-2 px-4 rounded-md hover:bg-[#BE6F50] focus:outline-none focus:bg-blue-600"
+              >
+                Sign up
+              </button>
+              <p className="text-white text-center mt-5">
+                Already have an account?{" "}
+                <Link to="/login">
+                  <span className="underline text-[#BE6F50]">Log in</span>{" "}
+                </Link>
+              </p>
+              <p className="text-white text-center mt-5">
+                Chef?{" "}
+                <Link to="/creatorSignup">
+                  <span className="underline text-[#BE6F50]">
+                    Sign up as content creator{" "}
+                  </span>
+                </Link>{" "}
+              </p>
+            </form>
+            <button className="mt-10 w-full flex items-center justify-center text-white bg-[#BE6F50] py-2 px-4 rounded-full hover:bg-[#8F8F8F] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+              <FaGoogle className="mr-2" /> Sign up with Google
             </button>
-
-            {/* Link to login page */}
-            <p className="text-white text-center mt-5">
-              Already have an account?{" "}
-              <Link to="/login">
-                <span className="underline text-[#BE6F50]">Log in</span>{" "}
-              </Link>
-            </p>
-
-            {/* Link to Content Creator page */}
-            <p className="text-white text-center mt-5">
-              Chef?{" "}
-              <Link to="/creatorSignup">
-                <span className="underline text-[#BE6F50]">
-                  Sign up as content creator{" "}
-                </span>
-              </Link>{" "}
-            </p>
-          </form>
-
-          {/* Signup with Google button */}
-          <button className="mt-10 w-full flex items-center justify-center text-white bg-[#BE6F50] py-2 px-4 rounded-full hover:bg-[#8F8F8F] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-            <FaGoogle className="mr-2" /> Sign up with Google
-          </button>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
